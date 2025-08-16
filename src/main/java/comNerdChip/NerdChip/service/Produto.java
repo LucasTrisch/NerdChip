4a5c6a963f0b1efb584efa6ec044f2925f8b5e2b
LucasTrisch <lucastrisch6@gmail.com>
2025-08-15 21:25:35 -0300

    update produto e pedido. falta colocar os metodos no service chamando o repository

--- src/main/java/comNerdChip/NerdChip/service/Produto.java
+++ src/main/java/comNerdChip/NerdChip/service/Produto.java
@@ -1,6 +1,7 @@
 package comNerdChip.NerdChip.service;
 
-import lombok.RequiredArgsConstructor;
+import comNerdChip.NerdChip.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,38 +12,36 @@ import comNerdChip.NerdChip.models.Produto;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/produtos")
-public class Produto  {
 
-    private  ProdutoService produtoService;
 
+public class ProdutoService  {
+    @Autowired
+    private ProdutoRepository produtoRepository;
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> listarTodos() {
-        return ResponseEntity.ok(produtoService.listarTodos());
+        return ResponseEntity.ok(produtoRepository.listarTodos());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProdutoDTO> buscarPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(produtoService.buscarPorId(id));
+        return ResponseEntity.ok(produtoRepository.buscarPorId(id));
     }
 
     @PostMapping
     public ResponseEntity<ProdutoDTO> criar(@Valid @RequestBody ProdutoCreateDTO produtoDTO) {
-        ProdutoDTO novoProduto = produtoService.criar(produtoDTO);
+        ProdutoDTO novoProduto = produtoRepository.criar(produtoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoProduto);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ProdutoDTO> atualizar(@PathVariable Long id, @Valid @RequestBody ProdutoCreateDTO produtoDTO) {
-        ProdutoDTO produtoAtualizado = produtoService.atualizar(id, produtoDTO);
+        ProdutoDTO produtoAtualizado = produtoRepository.atualizar(id, produtoDTO);
         return ResponseEntity.ok(produtoAtualizado);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        produtoService.deletar(id);
+        produtoRepository.deletar(id);
         return ResponseEntity.noContent().build();
     }
 }
