c7b6b5cc617744c6218afe68b5e153f0a80b904d
LucasTrisch <lucastrisch6@gmail.com>
2025-08-25 21:01:18 -0300

    atualizaçãode segunda

--- src/main/java/comNerdChip/NerdChip/repositories/UsuarioRepository.java
+++ src/main/java/comNerdChip/NerdChip/repositories/UsuarioRepository.java
@@ -5,20 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import comNerdChip.NerdChip.dtos.UsuarioDTO;
 import comNerdChip.NerdChip.models.Produto;
 @Repository
 
 public interface UsuarioRepository<Usuario> extends JpaRepository<Produto, Long> {
     Optional<Produto> findByDescricao(String descricao);
 
-    static UsuarioDTO atualizar(long id, UsuarioDTO usuarioDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
-    }
-
-    static UsuarioDTO criar(UsuarioDTO usuarioDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'criar'");
-    }
 }
